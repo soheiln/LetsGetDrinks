7e67519183155b7e88193501a986f7c4d305623a
Soheil Norouzi <soheil.nrz@gmail.com>
2016-05-31 13:38:37 -0700

    Add fetched results controller to collection view and show loaded venues in collection view

--- LetsGetDrinks/CollectionViewController.swift
+++ LetsGetDrinks/CollectionViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class CollectionViewController: UIViewController {
     
@@ -14,8 +15,17 @@ class CollectionViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
-    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+
+    var context: NSManagedObjectContext!
+    var fetchedResultsController : NSFetchedResultsController! {
+        // property observer
+        didSet{
+            executeSearch()
+            collectionView.reloadData()
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +35,40 @@ class CollectionViewController: UIViewController {
     
     func initView() {
         hideActivityIndicator()
-        //TODO
+        collectionView.dataSource = self
+        collectionView.delegate = self
+//        fetchedResultsController.delegate = self
+        context = CoreDataStack.sharedInstance().managedObjectContext
+        initCollectionView()
+        initFetchedResultsController()
+        
+        //TODO: remove
         GoogleClient.getVenuesNearLocation(callerViewController: self, latitude: 37.7749, longitude: -122.4194, errorHandler: nil,  completionHandler: { venue in
             print(venue)
             CoreDataStack.sharedInstance().venues.append(venue)
-            
         })
     }
     
+    
+    // Initializes collection view
+    func initCollectionView() {
+        collectionView.backgroundColor = UIColor.whiteColor()
+        let screenSize = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let space: CGFloat = CGFloat(Constants.CollectionView.SpaceBetweenSections)
+        let numSection = Constants.CollectionView.NumSectionsInPortraitMode
+        let dimension = (screenWidth - (CGFloat(numSection-1) * space)) / CGFloat(numSection)
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+
+    
     @IBAction func favoritesButtonPressed(sender: AnyObject) {
+        //TODO: check
+//        showVenuesInCollectionView()
+        
+
     }
     
     
@@ -47,6 +82,74 @@ class CollectionViewController: UIViewController {
         activityIndicator.hidesWhenStopped = true
         activityIndicator.stopAnimating()
     }
+    
+    
+    // Loads the collection view with loaded venues
+    func initFetchedResultsController() {
+        let fetchRequest = NSFetchRequest(entityName: "Venue")
+//        let predicate = NSPredicate(format: "pin = %@", argumentArray: [pin])
+//        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "latitude", ascending: false)]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+    }
 
+
+}
+
+
+// MARK: - Fetched Results Controller Delegate
+extension CollectionViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        fetchedResultsController = controller
+        executeSearch()
+        collectionView.reloadData()
+    }
+    
+    func executeSearch() {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Error while trying to perform a search: \n\(error)\n\(fetchedResultsController)")
+        }
+    }
+    
+}
+
+
+// MARK: - Collection View Controller Delegate and Data Source
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if fetchedResultsController == nil {
+            return 0
+        } else {
+            return fetchedResultsController.sections![section].numberOfObjects
+        }
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("VenueCollectionViewCell", forIndexPath: indexPath) as! VenueCollectionViewCell
+        let venues = fetchedResultsController.fetchedObjects as! [Venue]
+        let venue = venues[indexPath.row]
+        if let photo = venue.photo {
+            cell.imageView.image = UIImage(data: venue.photo!)
+        } else {
+            cell.imageView.image = UIImage(named: "no_image_available")
+        }
+//        cell.imageView.image = UIImage(data: venue.photo!)
+        cell.label.text = venue.name
+        return cell
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! VenueCollectionViewCell
+        //TODO
+        
+    }
+    
 }
 
---
+++ LetsGetDrinks/VenueCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VenueCollectionViewCell.swift
+//  LetsGetDrinks
+//
+//  Created by soheiln on 5/31/16.
+//  Copyright © 2016 soheiln. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VenueCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
